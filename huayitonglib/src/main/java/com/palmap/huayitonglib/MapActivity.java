7f26259af8c146f66f4af8391ac3418c009bfc2c
yunyun.zhou <yunyun.zhou@palmaplus.com>
2017-12-19 14:38:24 +0800

    提交

--- huayitonglib/src/main/java/com/palmap/huayitonglib/MapActivity.java
+++ huayitonglib/src/main/java/com/palmap/huayitonglib/MapActivity.java
@@ -2,6 +2,7 @@ package com.palmap.huayitonglib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    //-------------------界面点击事件
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.back_rr) {
+            finish();
+        }
     }
 }
