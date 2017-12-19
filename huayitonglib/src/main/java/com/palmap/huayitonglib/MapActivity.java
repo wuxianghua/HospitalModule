c25c17fee098245bf74c46d51ce7258732a58e28
yunyun.zhou <yunyun.zhou@palmaplus.com>
2017-12-19 16:24:53 +0800

    提交

--- huayitonglib/src/main/java/com/palmap/huayitonglib/MapActivity.java
+++ huayitonglib/src/main/java/com/palmap/huayitonglib/MapActivity.java
@@ -19,4 +19,48 @@ public class MapActivity extends AppCompatActivity {
             finish();
         }
     }
+    //------------切换楼层点击按钮
+    public void onChangeFloorClick(View view) {
+        int i = view.getId();
+        if (i== R.id.b2){
+
+        }else  if (i== R.id.b1){
+
+        }else if (i== R.id.f0){
+            //平面图------------
+            
+        }else if (i== R.id.f1){
+
+        }else if (i== R.id.f2){
+
+        }else if (i== R.id.f3){
+
+        }else if (i== R.id.f4){
+
+        }else if (i== R.id.f5){
+
+        }else if (i== R.id.f6){
+
+        }else if (i== R.id.f7){
+
+        }else if (i== R.id.f8){
+
+        }else if (i== R.id.f9){
+
+        }else if (i== R.id.f10){
+
+        }else if (i== R.id.f11){
+
+        }else if (i== R.id.f12){
+
+        }else if (i== R.id.f13){
+
+        }else if (i== R.id.f14){
+
+        }else if (i== R.id.f15){
+
+        }
+    }
+
+
 }
