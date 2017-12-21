60eae0b748ffc184df9d173a4ff00d29c4ee9b55
杨金皇 <jhyang_1993@163.com>
2017-12-21 17:31:51 +0800

    添加讯飞语音合成相关类及接口

--- huayitonglib/src/main/java/com/palmap/huayitonglib/App.java
+++ huayitonglib/src/main/java/com/palmap/huayitonglib/App.java
@@ -2,6 +2,7 @@ package com.palmap.huayitonglib;
 
 import android.app.Application;
 
+import com.iflytek.cloud.SpeechUtility;
 import com.palmap.huayitonglib.db.bridge.DbConnectionManager;
 
 /**
@@ -14,12 +15,22 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         initDataBase();
+        initSpeechSDK();
     }
 
     /**
      * 初始化数据库
      */
-    private void initDataBase(){
+    private void initDataBase() {
         DbConnectionManager.get().init(this);
     }
+
+    /**
+     * 初始化语音SDK
+     */
+    private void initSpeechSDK() {
+        // TODO: 2017/12/21 改成接口
+        SpeechUtility.createUtility(this, "appid=" + BuildConfig.APP_ID);
+    }
+
 }
